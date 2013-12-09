e4b15706375630146dce95a92b03bbdbb778220f
Jerome <jerome@jolimont.fr>
2013-12-10 00:14:30 +0100

    Hardcode 2400 bps

--- firmware/RFM2Pi_RF12_Demo/TinySensor_RF12_Demo/TinySensor_RF12_Demo.ino
+++ firmware/RFM2Pi_RF12_Demo/TinySensor_RF12_Demo/TinySensor_RF12_Demo.ino
@@ -61,7 +61,7 @@ static byte value, stack[20], top, sendLen, dest, quiet;
 static byte testbuf[20], testCounter;
 
 
-#define RF12_DATA_RATE RF12_DATA_RATE_1
+#define RF12_DATA_RATE RF12_DATA_RATE_2
 
 static void saveConfig () {
     // save to EEPROM
